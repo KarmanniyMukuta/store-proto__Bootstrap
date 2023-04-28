13968451763794ca974d77588a99bf1bd203cf7e
KarmanniyMukuta <gen.nefchev@gmail.com>
2023-04-29 01:06:22 +0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const main = document.querySelector(".main")
+const gr = "1fr 1fr 1fr 1fr"
 function line(){
-    main.style.gridTemplateColumns = "$1fr";
+    main.style.gridTemplateColumns = "1fr";
 }
 
 function grid(){
-    main.style.gridTemplateColumns ="$grid";
+    main.style.gridTemplateColumns = gr;
 }
+
+
+const al = () => {
+    let alr = confirm("hi");
+    alert(alr);
+}
+
+
+
